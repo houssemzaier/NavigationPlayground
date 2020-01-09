5c62aed693b6b1120884a36d63a9e346d8e1fc86
houssemzaier <houssem.zaier@gmail.com>
2020-01-09 16:54:55 +0100

    [nav-23] fix tests

--- app/src/androidTest/java/com/bravedroid/navigation/ChooseRecipientFragmentTest.kt
+++ app/src/androidTest/java/com/bravedroid/navigation/ChooseRecipientFragmentTest.kt
@@ -2,6 +2,8 @@ package com.bravedroid.navigation
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -11,7 +13,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.*
 import org.mockito.Mockito
+import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
 class ChooseRecipientFragmentTest {
@@ -35,8 +39,34 @@ class ChooseRecipientFragmentTest {
 
         onView(withId(R.id.next_btn_choose_recipient)).perform(click())
 
-        Mockito.verify(mockNavController).navigate(
-            ChooseRecipientFragmentDirections.actionChooseRecipientFragmentToSpecifyAmountFragment("Jim")
+
+        verify(mockNavController).navigate(
+//            we can always use argsThat
+//            argThat<NavDirections> {
+//                it.arguments.getString("name") == "Jim"
+//            },
+//            we can use it because it is in kotlin as it is a data class, so equals is overridden
+//            we have used eq() because we needed a matchers later, we cannot mix matcher with real args https://stackoverflow.com/questions/24468456/invalid-use-of-argument-matchers/24468549
+            eq(
+                ChooseRecipientFragmentDirections.actionChooseRecipientFragmentToSpecifyAmountFragment(
+                    "Jim"
+                )
+            ),
+//            other matcher (but useless)
+//            any<NavDirections>(),
+            //            we can always use argsThat
+            argThat<NavOptions> {
+                it.enterAnim == (R.anim.slide_in_from_right_to_left)
+                        && it.exitAnim == (R.anim.slide_out_from_right_to_left)
+                        && it.popEnterAnim == (R.anim.slide_in_from_left_to_right)
+                        && it.popExitAnim == (R.anim.slide_out_from_left_to_right)
+            }
+//        we cannot use it because NavOptions is writen in java and has no equals overriden
+//            NavOptions.Builder()
+//                .setEnterAnim(R.anim.slide_in_from_right_to_left)
+//                .setExitAnim(R.anim.slide_out_from_right_to_left)
+//                .setPopEnterAnim(R.anim.slide_in_from_left_to_right)
+//                .setPopExitAnim(R.anim.slide_out_from_left_to_right).build()
         )
     }
 }
